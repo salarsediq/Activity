88824db9b3209fe27b4f2494521a0a0ce8076d4f
salarsediq <104266562+salarsediq@users.noreply.github.com>
2022-07-03 18:07:51 +0300

    Add files via upload

---
+++ Activityy1.java
@@ -0,0 +1,100 @@
+//import java.util.*;
+//public class Activityy1 {
+//
+//    public static void main(String[] args) {
+//        Scanner sc =new Scanner(System.in);
+//        System.out.println("enter the number");
+//        int x=sc.nextInt();
+//  evenOrOdd(x);
+//    }
+//   public static void evenOrOdd (int x) {
+//    if (x%2==0)
+//        System.out.println("the number is even");
+//    else 
+//        System.out.println("the number is odd"); 
+//
+//}
+//}
+
+
+
+//import java.util.*;
+//public class Activityy1 {
+//
+//	public static void main(String[] args) {
+//        Scanner sc =new Scanner(System.in);
+//        System.out.println("enter the number");
+//        int x=sc.nextInt();
+//        fibonacci(x);
+//	}
+//
+// public static void fibonacci(int f) {
+// int num1 =0;
+//  int num2 =1;
+//    for(int i=1; i<=f;i++) {
+//        System.out.print(num1+",");
+//        int sum = num1+num2;
+//        num1=num2;
+//        num2=sum;
+//     }
+//
+// }
+//}
+
+
+
+
+
+//import java.util.*;
+//public class Activityy1 {
+//
+//	public static void main(String[] args) {
+//		Scanner sc =new Scanner(System.in);
+//		System.out.println("enter the number");
+//		int x = sc.nextInt();
+//		 factorial(x);
+//	}
+//		public static int factorial(int x) {
+//			int factorial=1;
+//		    for(int counter=1;counter<=x;counter++){
+//		        factorial=factorial*counter;
+//		       } System.out.print("the factorial of "+x+" is "+factorial);
+//		    
+//		    return factorial;
+//		    }
+//		
+//}
+
+
+
+//import java.util.*;
+//public class Activityy1 {
+//
+//    public static void main(String[] args) {
+//        Scanner sc =new Scanner(System.in);
+//        System.out.println("enter first number");
+//        int x=sc.nextInt();
+//        System.out.println("enter second number");
+//        int y=sc.nextInt();
+//        System.out.println("enter third number if you only have two numbers enter 0");
+//        int z=sc.nextInt();
+//        if (z==0) 
+//             sum(x,y);
+//        else
+//            sum(x,y,z);
+//
+//  }
+//
+//      public static void sum (int firstNum , int secondNum) {
+//       int sum = firstNum + secondNum ;
+//       System.out.println("the sum of the numbers is "+sum);
+//    }
+//   public static void sum (int firstNum , int secondNum , int thirdNum) {
+//       int sum = firstNum + secondNum + thirdNum ;
+//       System.out.println("the sum of the numbers is "+sum);
+//}
+//   public static void sum (double firstNum , int secondNum) {
+//       double sum = firstNum + secondNum  ;
+//       System.out.println("the sum of the numbers is "+sum);
+//}
+//}
